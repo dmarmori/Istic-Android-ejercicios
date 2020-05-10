8c9b7423986d5f1ec101e232927eb828081ba7f5
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-10 16:03:08 -0300

    Azar1

--- JuegoNumAzar/app/src/main/java/com/example/juegonumazar/MainActivity.kt
+++ JuegoNumAzar/app/src/main/java/com/example/juegonumazar/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.juegonumazar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,10 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        btnCambiarNombre.setOnClickListener{
+        btnAdivinar.setOnClickListener{
             lblNombre.text = numeroSecreto.toString()
 
-            if(numeroSecreto.toString() == txtNombre.text.toString()){
+            if(numeroSecreto.toString() == txtIngNum.text.toString()){
                 Toast.makeText(this,"Crak ", Toast.LENGTH_LONG).show()
 
             }else
