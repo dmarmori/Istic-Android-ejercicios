eea7e4d5cae124695f56722c484b7ad7e0ad2705
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-10 15:12:00 -0300

    Azar

---
+++ JuegoNumAzar/app/src/main/java/com/example/juegonumazar/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.juegonumazar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val numeroSecreto=(Math.random() * 100).toInt()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnCambiarNombre.setOnClickListener{
+            lblNombre.text = numeroSecreto.toString()
+
+            if(numeroSecreto.toString() == txtNombre.text.toString()){
+                Toast.makeText(this,"Crak ", Toast.LENGTH_LONG).show()
+
+            }else
+            {
+                Toast.makeText(this,"No es el numero ", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+}
