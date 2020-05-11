28c6d1144ccdc3cff8622a35ca2573c31076b5df
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-10 21:38:46 -0300

    Azar7

--- JuegoNumAzar/app/src/main/java/com/example/juegonumazar/MainActivity.kt
+++ JuegoNumAzar/app/src/main/java/com/example/juegonumazar/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.juegonumazar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Toast
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     var vidas:Int = 5
     var intentos:Int = 0
+    var ayuda:Int = 1
+    var numeroMultiplicado:Int = 0
 
 
 
@@ -27,18 +28,48 @@ class MainActivity : AppCompatActivity() {
         txtIntentos.text = "Numero de intentos: $intentos"
 
         btnAyuda.setOnClickListener{
+                when(ayuda)
+                {
+                    1 ->
+                        if (numeroSecreto <= 50)
+                        {
+                            Toast.makeText(this, "Numero Menor a 51 ", Toast.LENGTH_LONG).show()
+                        } else
+                        {
+                            Toast.makeText(this, "Numero mayor a 50 ", Toast.LENGTH_LONG).show()
+                        }
 
-            if(numeroSecreto <= 50)
-            {
-                Toast.makeText(this,"Numero Menor a 51 ", Toast.LENGTH_LONG).show()
-            }else
-            {
-                Toast.makeText(this,"Numero mayor a 50 ", Toast.LENGTH_LONG).show()
-            }
+                    2 ->
+                        if (numeroSecreto %2 == 0)
+                        {
+                            Toast.makeText(this, "El numero es PAR", Toast.LENGTH_LONG).show()
+                        }else
+                        {
+                            Toast.makeText(this, "El numero es IMPAR", Toast.LENGTH_LONG).show()
+                        }
+                    3 ->
+                    {
+                        numeroMultiplicado = (numeroSecreto * 2 * 2 * 2 * 2)/2
+                        Toast.makeText(this, "(Numero * 2 * 2 * 2 * 2)/2 = $numeroMultiplicado", Toast.LENGTH_LONG).show()
+                    }
+                    else ->
+                    {
+                        Toast.makeText(this, "(Numero * 2 * 2 * 2 * 2)/2 = $numeroMultiplicado", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+            ayuda = ayuda + 1
         }
 
         btnMeRindo.setOnClickListener{
            Toast.makeText(this,"El numero es: $numeroSecreto" , Toast.LENGTH_LONG).show()
+            btnAdivinar.isEnabled = false
+            btnAyuda.isEnabled = false
+            txtIngNum.isEnabled = false
+            txtResFinal.text = "ABANDONASTE COMO RIBER"
+            txtResFinal.visibility = View.VISIBLE
+            txtResFinal.setTextColor(txtResFinal.getContext().getResources().getColor(R.color.colorPrimary))
         }
 
 
@@ -46,14 +77,24 @@ class MainActivity : AppCompatActivity() {
 
          if (vidas > 1) {
              if (numeroSecreto.toString() == txtIngNum.text.toString()) {
-                 Toast.makeText(this, "Crak ", Toast.LENGTH_LONG).show()
+                 if (ayuda < 3) {
+                     Toast.makeText(this, "Ganaste con ayudin ", Toast.LENGTH_LONG).show()
+                     txtResFinal.text = "GANASTE!! SEGURO SOS DE BOCA"
+                 }else
+                 {
+                     Toast.makeText(this, "Ganaste Crak ", Toast.LENGTH_LONG).show()
+                     txtResFinal.text = "GANASTE CON AYUDIN COMO RIBER"
+                 }
                  vidas = vidas - 1
                  intentos = intentos + 1
                  txtVidas.text = "Vidas restantes: " + vidas
                  txtIntentos.text = "Numero de intentos: " + intentos
                  btnAdivinar.isEnabled = false
                  btnAyuda.isEnabled = false
-                 txtGanaste.visibility = View.VISIBLE
+                 btnMeRindo.isEnabled = false
+                 txtIngNum.isEnabled = false
+                 txtResFinal.visibility = View.VISIBLE
+
 
              } else {
                  Toast.makeText(this, "No es el numero ", Toast.LENGTH_LONG).show()
@@ -70,7 +111,13 @@ class MainActivity : AppCompatActivity() {
              txtVidas.text = "Vidas restantes: " + vidas
              txtIntentos.text = "Numero de intentos: " + intentos
              btnAdivinar.isEnabled = false
+             btnMeRindo.isEnabled = false
              btnAyuda.isEnabled = false
+             txtIngNum.isEnabled = false
+             txtResFinal.text = "PERDISTE!!! SEGURO SOS DE RIBER"
+             txtResFinal.visibility = View.VISIBLE
+             txtResFinal.setTextColor(txtResFinal.getContext().getResources().getColor(R.color.colorPrimary))
+
          }
 
         }
