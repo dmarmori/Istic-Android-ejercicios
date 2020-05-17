c499e694ea2d58d3731bf6fb34837ac43fd5694d
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-17 19:56:57 -0300

    FinalContadorLogica

--- 1erParcial/app/src/main/java/com/example/a1erparcial/Contador.kt
+++ 1erParcial/app/src/main/java/com/example/a1erparcial/Contador.kt
@@ -2,13 +2,25 @@ package com.example.a1erparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contador.*
 
 class Contador : AppCompatActivity() {
+
+    var contador:Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contador)
 
+        btnContar.setOnClickListener {
+            contador = contador +1
+            lblContador.text = contador.toString()
+        }
+
+        btnReserContador.setOnClickListener {
+            contador = 0
+            lblContador.text = ""
+        }
 
     }
 }
