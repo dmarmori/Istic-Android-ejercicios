a147ba575ccb1128144c16f59b4cd711bc7c139b
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-17 19:12:56 -0300

    Contador

---
+++ 1erParcial/app/src/main/java/com/example/a1erparcial/Contador.kt
@@ -0,0 +1,14 @@
+package com.example.a1erparcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contador)
+
+
+    }
+}
--- 1erParcial/app/src/main/java/com/example/a1erparcial/Menu.kt
+++ 1erParcial/app/src/main/java/com/example/a1erparcial/Menu.kt
@@ -1,12 +1,19 @@
 package com.example.a1erparcial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class Menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        btnContador.setOnClickListener {
+            val ContadorIntent = Intent(this, Contador::class.java)
+            startActivity(ContadorIntent)
+        }
     }
 }
