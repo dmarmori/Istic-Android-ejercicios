ee9a4a5318dd88f30d6b432dee7934ec2b033524
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-19 22:07:35 -0300

    un commit mas

--- 1erParcial/app/src/main/java/com/example/a1erparcial/MainActivity.kt
+++ 1erParcial/app/src/main/java/com/example/a1erparcial/MainActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.widget.Toast
 import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Thread.sleep(1000)
+        setTheme(R.style.AppTheme)//Tema para splashscreem
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
