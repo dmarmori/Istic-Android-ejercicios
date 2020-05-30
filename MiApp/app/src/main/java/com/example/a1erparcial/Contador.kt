7e359bb7b1224c722e7b9a4318f922d67d7c87dc
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-05-30 17:40:13 -0300

    NuevaPantalla

--- MiApp/app/src/main/java/com/example/a1erparcial/Contador.kt
+++ MiApp/app/src/main/java/com/example/a1erparcial/Contador.kt
@@ -3,6 +3,9 @@ package com.example.a1erparcial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_contador.*
 
 class Contador : AppCompatActivity() {
@@ -13,24 +16,34 @@ class Contador : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contador)
 
-        btnSumar.setOnClickListener {
+        val botonSumar = findViewById<Button>(R.id.btnSumar) //Declaracion para boton ingresar
+        botonSumar.setOnClickListener {
             contador = contador +1
             lblContador.text = contador.toString()
         }
 
-        btnRestar.setOnClickListener {
+        val botonRestar = findViewById<Button>(R.id.btnRestar) //Declaracion para boton ingresar
+        botonRestar.setOnClickListener {
             contador = contador -1
             lblContador.text = contador.toString()
         }
 
-        btnReserContador.setOnClickListener {
+        val botonContar = findViewById<Button>(R.id.btnReserContador) //Declaracion para boton ingresar
+        botonContar.setOnClickListener {
             contador = 0
             lblContador.text = ""
         }
 
-        lblayuda.setOnClickListener {
+        val botonAyuda = findViewById<TextView>(R.id.lblayuda) //Declaracion para boton ingresar
+        botonAyuda.setOnClickListener {
             val ayudaIntent= Intent(this, ComoJugarContador::class.java)
             startActivity(ayudaIntent)
+        }
+
+        val botonDatosCont = findViewById<ImageView>(R.id.imgGuardarCont) //Declaracion para boton ingresar
+        botonDatosCont.setOnClickListener {
+            val guardarDatosIntent= Intent(this, DatosContador::class.java)
+            startActivity(guardarDatosIntent)
         }
 
     }
---
+++ MiApp/app/src/main/java/com/example/a1erparcial/DatosContador.kt
@@ -0,0 +1,12 @@
+package com.example.a1erparcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatosContador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_contador)
+    }
+}
